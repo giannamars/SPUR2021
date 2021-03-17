7bad874ab5be73837e44313b96fb8cbc47becd54
Gianna Marschmann <giannamars@gmail.com>
2021-03-17 14:29:39 -0700

    added more files

--- scripts/iYO844.m
+++ scripts/iYO844.m
@@ -1,18 +1,14 @@
+clear all
+
 % load in the file
 fileName = 'iYO844.mat';
 if ~exist('modelOri','var')
-model = readCbModel(fileName);
+modelOri = readCbModel(fileName);
 end
 
-% check if reactions bounds are set to -/+ 1000
-% if not, how to change them? doc changeRxnBounds
-printConstraints(model)
+model = modelOri;
 
-% set maximum biomass production as the objective function
-% need to change this line to identify the biomass reaction in the mat file
-%model = changeObjective(model,'Biomass_Ecoli_core_N(w/GAM)-Nmet2');
+model = changeObjective(model,'BIOMASS_BS_10');
 
-model.rxnNames
-
-
+FBAsolution = optimizeCbModel(model,'max');
 
